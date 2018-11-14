84caa1b260f6836827401f316db6aaf74bb08790
alex sor kang <asork42@berkeley.edu>
2018-11-13 17:41:59 -0800

    Updates

---
+++ precache-manifest.c838105466cbb466c2fc49fa88410a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fead7a98ec7f69bc126",
+    "url": "/m4abun/static/css/main.9a5adac6.chunk.css"
+  },
+  {
+    "revision": "3fead7a98ec7f69bc126",
+    "url": "/m4abun/static/js/main.3fead7a9.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/m4abun/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d015db97864e0f8a8c5f",
+    "url": "/m4abun/static/js/runtime~main.d015db97.js"
+  },
+  {
+    "revision": "970e7da701841cce3585b4e313517d8a",
+    "url": "/m4abun/static/media/kiru.970e7da7.png"
+  },
+  {
+    "revision": "e3dc4395eaa3c58497f8439125a274ef",
+    "url": "/m4abun/index.html"
+  }
+];
