13cb3a5d785b9d482c85082148fa2a7aef231e56
alex sor kang <asork42@berkeley.edu>
2018-11-14 12:44:02 -0800

    timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import logo from './kiru.png';
 import './App.css';
+import moment from 'moment';
+import humanizeDuration from 'humanize-duration';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const a = moment('2016-11-06 07:39:00')
+    const b = moment()
+    const duration = moment.duration(b.diff(a))
+    this.state = { date: humanizeDuration(duration, { round: true }) };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+        () => this.tick(),
+        1000
+    );
+  }
+  componentWillUnmount() {
+      clearInterval(this.timerID);
+  }
+  tick() {
+    const a = moment('2016-11-06 07:39:00')
+    const b = moment()
+    const duration = moment.duration(b.diff(a))
+    this.setState({
+        date: humanizeDuration(duration, { round: true })
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            
+            {this.state.date} since kiru promised
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn More
-          </a>
         </header>
       </div>
     );
